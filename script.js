d47225f36d36353c45970a9702249c740e356d6d
jamesboblak <james@skepticalrecords.com>
2021-06-24 05:30:32 -0700

    added variables and user prompts

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Character options
 var capChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var nocapChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var specChars = []
+var specChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+var passChars = []
 
+// User questions
+var passDigits = prompt("How many digits would you like (8-72?")
+var capSelect = confirm("Would you like to use uppercase letters?");
+var noCapSelect = confirm("Would you like to use lowercase letters?");
+var numSelect = confirm("Would you like to use numbers?");
+var specSelect = confirm("Would you like to use special characters?");
+if (capSelect === true) {
+  passChars = passChars.concat(capChars);
+}
+if (noCapSelect === true) {
+  passChars = passChars.concat(nocapChars);
+}
+if (numSelect === true) {
+  passChars = passChars.concat(numChars);
+}
+if (specSelect === true) {
+  passChars = passChars.concat(specChars);
+}
+
+console.log(passChars);
+console.log(passDigits);
 
 // Write password to the #password input
 function writePassword() {
