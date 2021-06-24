f80ff8a152748bf994cbfdfb20a3e8625b7a6683
jamesboblak <james@skepticalrecords.com>
2021-06-24 07:03:06 -0700

    started commenting JS

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Character options
+// Character options ================
+// Uppercase letters
 var capChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Lowercase Letters
 var nocapChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Numbers
 var numChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Special characters
 var specChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+//Placeholder to build chac=racter set in
 var passChars = [];
+
+//Placeholder to hold new password as an array
 var newPass = [];
 
 // User questions
